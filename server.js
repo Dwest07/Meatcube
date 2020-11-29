e7aa5e2a8296b717bb16a233f690368cc309cbaa
Dwest07 <59906959+Dwest07@users.noreply.github.com>
2020-11-29 16:05:39 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,4 +3,11 @@ var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+
+
 app.listen(3000);
