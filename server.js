e3ed3e65dc893f833f27ccede17969277640f95e
Dwest07 <59906959+Dwest07@users.noreply.github.com>
2020-11-29 16:02:36 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.listen(3000);
